b819550001dbef4be8f353ab08cb914cce47a6c4
o0Maikeru0o <mnickandros@gmail.com>
2019-07-23 13:50:02 -0400

    icreased test coverage for nearby component

--- client/__Tests__/Nearby.test.js
+++ client/__Tests__/Nearby.test.js
@@ -2,25 +2,42 @@
 import axios from 'axios';
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
-import { create } from 'react-test-renderer';
 import mockData from './mockData.json';
 import Nearby from '../components/Nearby.jsx';
 
 const wrapper = mount(<Nearby />);
 // const shallowWrapper = shallow(<Nearby />);
 jest.mock('axios');
+global.console.log = jest.fn();
+
 
 describe('Nearby.jsx component', () => {
+  const response = { data: mockData };
+  const instance = wrapper.instance();
   it('Should display "Loading..." prior to fetching data', () => {
     expect(wrapper.text()).toContain('Loading...');
   });
 
-  it('It fetches data with componentDidMount()', async () => {
-    const response = { data: mockData };
-    axios.get.mockResolvedValue(response);
-    const component = create(<Nearby />);
-    const instance = component.getInstance();
-    await instance.componentDidMount();
-    console.log('erreeeeeeeeeeeeee', response);
+  it('Should render Carousel if data has been set in state', () => {
+    wrapper.setState({ carouselData: mockData });
+    expect(wrapper.find('Carousel').text().length).toBeGreaterThan(0);
+  });
+
+  describe('Nearby Methods', () => {
+    it('Fetches data in componentDidMount()', async () => {
+      axios.get.mockResolvedValue(response);
+      await instance.componentDidMount();
+    });
+
+    // it('Should log an Error on a bad fetch request', async () => {
+    //   axios.get.mockRejectedValue(new Error( 'Request failed with status code 400') );
+    //   await instance.fetchData();
+    //   expect(global.console.log).toEqual('Error: Request failed with status code 400');
+    // });
+
+    it('Adds favorites and updates state', async () => {
+      axios.put.mockResolvedValue(response);
+      await instance.addFavorite();
+    });
   });
 });
